4a84d540ac6bb3a0467459fb8b6f8022dcf95e7f
Amy Hanlon <amyehanlon@gmail.com>
2014-06-16 15:33:20 -0400

    draw board using js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
-})
+    var $table = $('<table></table>')
+    for (var i = 0; i < 3; i++){
+        $table.append($('<tr><td></td><td></td><td></td></tr>'))
+    };
+    $('div').append($table)
+});
