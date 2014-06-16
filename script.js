bea8a0d8d5e2f74ab8d53a4e446fce42fcefdc78
Amy Hanlon <amyehanlon@gmail.com>
2014-06-16 16:21:24 -0400

    xs and os!!!

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
+
+    var markSquare = (function(){
+            console.log(this);
+            var i = 0;
+            return function(){
+                i+=1;
+                if (i % 2 === 0){
+                    return 'X';
+                } else {
+                    return 'O';
+                }
+            }
+        });
+
     var board = function(){
         var $table = $('<table></table>')
         for (var i = 0; i < 3; i++){
             $table.append($('<tr><td></td><td></td><td></td></tr>'))
         };
         $('div').append($table)
-        $('td').on('click', function(event){
-            console.log(this);
-            $(this).css('background-color', 'green');
-        });
+        var mark = markSquare()
+        $('td').on('click', function(){
+            console.log(mark)
+            $(this).text(mark)
+        })
     };
     board();
 });
