b2cbf4c7a8172ff442ef49af9ee02e1416ba80a6
Amy Hanlon <amyehanlon@gmail.com>
2014-06-16 15:52:46 -0400

    turn the squares red!

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    var $table = $('<table></table>')
-    for (var i = 0; i < 3; i++){
-        $table.append($('<tr><td></td><td></td><td></td></tr>'))
+    var board = function(){
+        var $table = $('<table></table>')
+        for (var i = 0; i < 3; i++){
+            $table.append($('<tr><td></td><td></td><td></td></tr>'))
+        };
+        $('div').append($table)
     };
-    $('div').append($table)
+    board();
+    $('table').children().css('background-color', 'red');
 });
