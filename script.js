e1ca4b0ef7cd38a1b1aec44bbb770d286a29b192
Amy Hanlon <amyehanlon@gmail.com>
2014-06-19 21:59:30 -0400

    paired with mary, initial structure for model and view objects

--- script.js
+++ script.js
@@ -1,43 +1,82 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var markSquare = (function(){
-        var mark
-        return function(){
-            if (mark === 'X'){
-                mark = 'O'
-            } else {
-                mark = 'X'
-            }
-            return mark;
+    var view = {
+        tableId: 'board',
+
+        getTable: function() {
+            return $('#' + this.tableId);
+        },
+
+        setSquare: function(index, value) {
+            this.getTable().children()[index].text(value);
+        },
+
+        addSquare: function() {
+            this.getTable().append($('<td>'));
+        },
+
+        createTable: function() {
+            var $table = $('<table></table>');
+            $('#game').append($table);
+            $table.attr('id', this.tableId);
+        },
+
+        bindClickSquare: function(index, func){
+            this.getTable().children()[index].click(func);
         }
-    });
+    };
 
-    // var checkWinner = function(){
-    //     for (var i = 0; i < 3; i++){
-    //         var $row = $('tbody').children()[i]
-    //         for (var j = 0; j < 3; j++){
-    //             var $square = $row.children()[j]
-    //             $square
+    var model = {
+        data: [],
+
+        setSquare: function(index, value){
+            this.data[index] = value;
+            view.setSquare(index, value);
+        },
+
+        getSquare: function(index){
+            return this.data[index];
+        },
+
+        setUpBoard: function(){
+            view.createTable()
+            for (var i = 0; i < 9; i++){
+                this.data.push(undefined);
+                view.addSquare();
+                var self = this
+                view.bindClickSquare(i, function(){
+                    self.setSquare
+                });
+            }
+        }
+    };
+
+
+    model.setUpBoard()
+
+    // var markSquare = function(){
+    //     var mark;
+    //     return function(){
+    //         if (mark === 'X'){
+    //             mark = 'O';
+    //         } else {
+    //             mark = 'X';
     //         }
+    //         return mark;
     //     };
     // };
 
-    var board = function(){
-        var $table = $('<table></table>')
-        for (var i = 0; i < 3; i++){
-            $table.append($('<tr><td></td><td></td><td></td></tr>'))
-        };
-        $('div').append($table)
-        var mark = markSquare()
-        $('td').on('click', function(){
-            if ($(this).text()){
-                alert('invalid square')
-            }
-            else {
-                $(this).text(mark());
-                // checkWinner();
-            }
-        });
-    };
-    board();
+    // var board = function(){
+    //     var $table = $('<table></table>');
+    //     for (var i = 0; i < 3; i++){
+    //         $table.append($('<tr><td></td><td></td><td></td></tr>'));
+    //     }
+    //     $('div').append($table);
+    //     var mark = markSquare();
+    //     $('td').on('click', function(){
+    //         model.clickSquare(this.id)
+    //         }
+    //     });
+    // };
+    // board();
 });
